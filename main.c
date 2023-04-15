10351218774b75350fbb68dcaca229bda1d9e17d
monster energy <berenyibence98@gmail.com>
2023-04-16 01:12:55 +0200

    real

---
+++ main.c
@@ -0,0 +1,123 @@
+#include "raylib.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    int px = screenWidth/2;
+    int py = screenHeight/2;
+
+    bool isFuelSpawned = false;
+    
+    bool isPlaying = true;
+
+    double g = 0;
+
+    double fuel = 100.0;
+
+    int fuelValue = 10;
+
+    Rectangle playerRect = { px, py, 20, 20 };
+    Rectangle fuelRect = { GetRandomValue(0, screenWidth - 20), GetRandomValue(0, screenHeight - 20), 20, 20 };
+
+    InitWindow(screenWidth, screenHeight, "jetpack fella");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        if (IsKeyDown(KEY_RIGHT)){
+            px += 2;
+        }
+        if (IsKeyDown(KEY_LEFT)){
+            px -= 2;
+        }
+        if (IsKeyDown(KEY_SPACE) && fuel > 0){
+            g = 5;
+            fuel -= 0.5;
+            DrawText("*", px, py+5, 20, RED);
+        }
+
+        playerRect.x = px;
+        playerRect.y = py;
+
+        if (CheckCollisionRecs(playerRect, fuelRect)) {
+            if (fuel + fuelValue <= 100) {
+                fuel += fuelValue;
+            } else if (fuel + fuelValue >= 100) {
+                fuel = 100;
+            }
+            isFuelSpawned = false;
+        }
+
+        BeginDrawing();
+
+            ClearBackground(GRAY);
+
+            DrawRectangle(10,10,30,110,BLACK);
+            DrawRectangle(15,15,20,fuel,GREEN);
+
+            DrawRectangle(fuelRect.x, fuelRect.y, fuelRect.width, fuelRect.height, RED);
+
+            DrawText("o", px, py, 20, WHITE);
+
+            /* DrawText(TextFormat("x: %d", px), 700, 10, 20, WHITE);
+            DrawText(TextFormat("y: %d", py), 700, 30, 20, WHITE);
+            DrawText(TextFormat("playing: %d", isPlaying), 700, 50, 20, WHITE);
+            DrawText(TextFormat("fuel: %f", fuel), 700, 70, 20, WHITE); useless debug messages */
+
+            if (isPlaying == false) {
+                DrawRectangle(0,0,screenWidth,screenHeight,BLACK);
+
+                const char* text = "You Lost!";
+                const float textWidth = MeasureText(text, 50);
+                const float x = (screenWidth / 2.0f) - (textWidth / 2.0f);
+
+                DrawText(text, x, screenHeight/2 - 25, 50, WHITE);
+
+                const char* replayText = "Press space to replay";
+                const float replayTextWidth = MeasureText(replayText, 30);
+                const float replayTextX = (screenWidth / 2.0f) - (replayTextWidth / 2.0f);
+
+                DrawText(replayText, replayTextX, screenHeight/2 + 25, 30, WHITE);
+
+                if (IsKeyPressed(KEY_SPACE)) {
+                    fuel = 100.0;
+                    g = 0.0;
+                    px = screenWidth / 2;
+                    py = screenHeight / 2;
+                    isPlaying = true;
+                }
+            }
+
+        EndDrawing();
+
+        py -= g;
+        g -= 0.2;
+
+        if (px > screenWidth) {
+            px = 0;
+        }
+        else if (px < 0) {
+            px = screenWidth;
+        }
+
+        if (py <= 0) {
+            py = 0;
+        }
+
+        if (py > screenHeight) {
+            isPlaying = false;
+        }
+
+        if (!isFuelSpawned) {
+            fuelRect.x = GetRandomValue(50, screenWidth - 20);
+            fuelRect.y = GetRandomValue(20, screenHeight - 50);
+            isFuelSpawned = true;
+        }
+    }
+
+    CloseWindow();
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+make:
+	cc main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o game
